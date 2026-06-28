a24327c8ae3d85fab329291e7aae219b27192085
anis balkhi <anishbalkhi1@gmail.com>
2026-06-28 20:11:53 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://your-app-name.vercel.app'
+    'https://task-manager-4wii.vercel.app/'
   ]
 }));
 app.use(express.json());
